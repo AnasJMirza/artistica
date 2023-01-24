63d9d48032b30e44b4030448a3d24e0b70a95b41
AnasJMirza <ajmhimself@gmail.com>
2023-01-24 10:16:53 +0500

    axios-bug-fix

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://artistica-anasjmirza.vercel.app/',
+    baseURL: 'https://artistica.vercel.app/',
     headers: {'Content-Type': 'application/json'},
 });
 

2402485f5fb581f528d0bf8635b89c2bb10cd9c0
AnasJMirza <ajmhimself@gmail.com>
2023-01-24 12:11:46 +0500

    server-redeploy

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://artistica.vercel.app/',
+    baseURL: 'https://ill-ruby-peacock-gown.cyclic.app/',
     headers: {'Content-Type': 'application/json'},
 });
 

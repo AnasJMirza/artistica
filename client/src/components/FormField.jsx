e5897f59b4c42dbb229cd10857269cc3722c3b31
AnasJMirza <ajmhimself@gmail.com>
2023-01-24 08:32:00 +0500

    client-routing-bug-fixed

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -4,17 +4,17 @@ const FormField = (props) => {
   return (
     <div>
       <div className="flex items-center gap-2 mb-2">
-        <label className="block text-sm font-medium text-gray-900">
+        <label className="block text-sm font-medium text-gray-900"  htmlFor={props.name}>
           {props.labelName}
         </label>
         {
             props.isSurpriseMe && (
-                <button 
+                <div 
                 className="font-semibold text-xs bg-[#EcECF1] py-1 px-2 rounded-[5px] text-black"
                 onClick={props.handleSurpriseMe}
                 >
                     Surprise Me
-                </button>
+                </div>
             )
         }
       </div>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,9 +6,8 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 )

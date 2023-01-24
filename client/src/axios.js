67c8ad8eb55d53c6c74494d4ecd5359a46ae5f16
AnasJMirza <ajmhimself@gmail.com>
2023-01-24 12:14:26 +0500

    server-redeploy

--- client/src/axios.js
+++ client/src/axios.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
     baseURL: 'https://ill-ruby-peacock-gown.cyclic.app/',
     headers: {'Content-Type': 'application/json'},
+    withCredentials: true
 });
 
 
--- server/index.js
+++ server/index.js
@@ -15,7 +15,10 @@ const app = express();
 
 // middlewares
 
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: "https://artistica-by-anas.vercel.app/",
+}));
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);

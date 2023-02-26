1817719a66df4593a59c3751ec140a22fe002101
AnasJMirza <ajmhimself@gmail.com>
2023-02-27 00:39:05 +0500

    redeploy

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // baseURL: 'https://artistica-server.onrender.com',
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://artistica-server.onrender.com',
+    // baseURL: 'http://localhost:8080',
     headers: {'Content-Type': 'application/json'}
 });
 
